7106ad204bade5e66b2645b641203c910988089c
CIITSHSDTP00\CIIT <it@ciit.edu.ph>
2025-01-13 18:10:53 +0800

    Week 1 GameNetworking

--- Assets/Scripts/AddressableAssets.cs
+++ Assets/Scripts/AddressableAssets.cs
@@ -1,23 +1,59 @@
 using UnityEngine;
 using UnityEngine.AddressableAssets;
 using UnityEngine.ResourceManagement.AsyncOperations;
-public class AddressableAssets : MonoBehaviour
+
+[System.Serializable]
+public class AssetReferenceAudioClip : AssetReferenceT<AudioClip>
 {
+    public AssetReferenceAudioClip(string guid) : base(guid){
+
+    }
+}
+public class AddressableAssets : MonoBehaviour
+{   
+    [SerializeField] private AssetReferenceGameObject assetReferenceObject; //reference to addressable prefab path
+    [SerializeField] private AssetReferenceAudioClip assetReferenceAudioClip; //reference to addressable audioClip path
+    [SerializeField] private AudioSource audioSource;
+    private bool test;
+    GameObject addressableGameObjectContainer;
+    AudioClip addressableAudioContainer;
+
+    private void Start()
+    {
+        //load audio clip from addressable
+       assetReferenceAudioClip.LoadAssetAsync<AudioClip>().Completed +=
+        (AsyncOperationHandleComplete) => {
+            if (AsyncOperationHandleComplete.Status == AsyncOperationStatus.Succeeded){
+                addressableAudioContainer = audioSource.clip = AsyncOperationHandleComplete.Result;
+            }
+            else{
+                Debug.LogError("Failed To Load");
+            }
+        };
+
+        assetReferenceObject.LoadAssetAsync<GameObject>().Completed +=
+         (AsyncOperationHandleComplete1) =>
+         {
+             if (AsyncOperationHandleComplete1.Status == AsyncOperationStatus.Succeeded)
+             {
+                 addressableGameObjectContainer = AsyncOperationHandleComplete1.Result;
+             }
+             else
+             {
+                 Debug.LogError("Failed To Load");
+             }
+         };
+    }
     void Update(){
         if (Input.GetKeyDown(KeyCode.K)){
-            AsyncOperationHandle<GameObject> asyncOperationHandle =
-                Addressables.LoadAssetAsync<GameObject>("Assets/Prefabs/Cube.prefab");
-
-            asyncOperationHandle.Completed += AsyncOperationHandleComplete;
-        }       
-    }
-
-    private void AsyncOperationHandleComplete(AsyncOperationHandle<GameObject> asyncOperationHandle){
-        if (asyncOperationHandle.Status == AsyncOperationStatus.Succeeded){
-            Instantiate(asyncOperationHandle.Result);
+            Instantiate(addressableGameObjectContainer);
+            audioSource.Play();
         }
-        else{
-            Debug.LogError("Fail to Load");
+
+        if (Input.GetKeyDown(KeyCode.Backspace)){
+            assetReferenceObject.ReleaseInstance(addressableGameObjectContainer);
+            Destroy(addressableGameObjectContainer);
         }
+
     }
 }
